40a67212feccb2a5724a0230a4b738618a840902
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-04-29 21:32:20 -0700

    Fix app layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import SideNav from "@/components/side-nav.vue";
+import Header from "@/components/header.vue";
+</script>
+
 <template>
-  <RouterView />
+  <div class="grid min-h-screen w-full md:grid-cols-[200px_1fr] lg:grid-cols-[280px_1fr]">
+    <SideNav />
+    <div class="flex flex-col">
+      <Header />
+      <RouterView class="p-4"/>
+    </div>
+  </div>
 </template>
