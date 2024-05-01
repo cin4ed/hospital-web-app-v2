19aeb206d72c986b5bcc175378287de949c34389
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-05-01 10:30:42 -0700

    Create DataTableDropDown.vue

---
+++ src/components/DataTableDropDown.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+	import { MoreHorizontal } from 'lucide-vue-next';
+	import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+	import { Button } from '@/components/ui/button';
+
+	defineProps<{
+
+	}>();
+
+	function doSomething() {
+		console.log("hello");
+	}
+</script>
+
+<template>
+	<DropdownMenu>
+		<DropdownMenuTrigger as-Child>
+			<Button variant="ghost" class="w-8 h-8 p-0">
+				<span class="sr-only">Open menu</span>
+				<MoreHorizontal class="w-4 h-4" />
+			</Button>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent align="end">
+			<DropdownMenuLabel>Actions</DropdownMenuLabel>
+			<DropdownMenuItem @click="doSomething">
+				Do Something
+			</DropdownMenuItem>
+			<DropdownMenuSeparator />
+			<DropdownMenuItem>View customer</DropdownMenuItem>
+			<DropdownMenuItem>View payment</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>	
+</template
