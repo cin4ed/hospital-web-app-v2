b9ba58ccb4287088600e337e72d553476a0b357e
VictorMRD <victormrd2002@hotmail.com>
2024-05-21 16:17:04 -0700

    Add patients edit

---
+++ src/pages/patients/edit/index.vue
@@ -0,0 +1,165 @@
+<script setup lang="ts">
+  import { useForm } from "vee-validate";
+  import axios from "@/lib/axios";
+  import { toTypedSchema } from "@vee-validate/zod";
+  import * as z from "zod";
+
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb";
+
+  import {
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form";
+
+  import { Input } from "@/components/ui/input";
+
+  import { Button } from "@/components/ui/button";
+
+  const formSchema = toTypedSchema(
+    z.object({
+      name: z.string(),
+      lastname: z.string(),
+      birth_date: z.string(),
+      affiliation_date: z.string(),
+      phone_number: z.number(),
+      blood_type: z.enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]),
+    })
+  );
+
+  const form = useForm({
+    validationSchema: formSchema,
+  });
+
+  const onSubmit = form.handleSubmit((values) => {
+    axios.put(`/patients/${0}`, values);
+  });
+</script>
+
+<template>
+  <div class="space-y-2">
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <RouterLink to="/">Inicio</RouterLink>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/medicines">
+            <RouterLink to="/medicines">Medicamentos</RouterLink>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Nuevo</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+
+    <h1 class="text-3xl font-semibold">Agrega un nuevo paciente</h1>
+    <form @submit.prevent="onSubmit" class="space-y-4">
+      <!-- name -->
+      <FormField v-slot="{ componentField }" name="name">
+        <FormItem>
+          <FormLabel>Nombre</FormLabel>
+          <FormControl>
+            <Input
+              type="text"
+              placeholder="Nombre del paciente"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <!-- active ingredients -->
+      <FormField v-slot="{ componentField }" name="lastname">
+        <FormItem>
+          <FormLabel>Apellidos</FormLabel>
+          <FormControl>
+            <Input
+                type="text"
+                placeholder="Apellidos del paciente"
+                v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <!-- dosage strength -->
+      <FormField v-slot="{ componentField }" name="birth_date">
+        <FormItem>
+          <FormLabel>Fecha de nacimiento</FormLabel>
+          <FormControl>
+            <Input
+                type="date"
+                placeholder="500"
+                v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <!-- dosage unit -->
+      <FormField v-slot="{ componentField }" name="affiliation_date">
+        <FormItem>
+          <FormLabel>Fecha de afiliacion</FormLabel>
+          <FormControl>
+            <Input
+                type="date"
+                placeholder="mg"
+                v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <!-- prescription info -->
+      <FormField v-slot="{ componentField }" name="phone_number">
+        <FormItem>
+          <FormLabel>Número telefonico</FormLabel>
+          <FormControl>
+            <Input
+                type="number"
+                placeholder="placeholder"
+                v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <!-- presentation -->
+      <FormField v-slot="{ componentField }" name="blood_type">
+        <FormItem>
+          <FormLabel>Tipo de sangre</FormLabel>
+          <FormControl>
+            <select v-bind="componentField">
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <Button type="submit">Guardar</Button>
+    </form>
+  </div>
+</template>
