896f1c5fa383b92de6689257245dc996a6ca4f41
VictorMRD <victormrd2002@hotmail.com>
2024-05-21 18:08:15 -0700

    Add redirection to index on create patient

--- src/pages/patients/new/index.vue
+++ src/pages/patients/new/index.vue
@@ -3,6 +3,7 @@ import { useForm } from "vee-validate";
 import axios from "@/lib/axios";
 import { toTypedSchema } from "@vee-validate/zod";
 import * as z from "zod";
+import { useRouter } from 'vue-router';
 
 import {
   Breadcrumb,
@@ -28,22 +29,24 @@ import { Button } from "@/components/ui/button";
 
 const formSchema = toTypedSchema(
   z.object({
-    name: z.string(),
-    lastname: z.string(),
-    birth_date: z.string(),
-    affiliation_date: z.string(),
-    phone_number: z.number(),
-    blood_type: z.enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]),
-  })
-);
+      name: z.string(),
+      lastname: z.string(),
+      birth_date: z.string(),
+      affiliation_date: z.string(),
+      phone_number: z.number(),
+      blood_type: z.enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]),
+    })
+  );
 
   const form = useForm({
     validationSchema: formSchema,
   });
 
+  const router = useRouter();
   const onSubmit = form.handleSubmit((values) => {
     console.log(values);
     axios.post(`/patients`, values);
+    router.push(`/patients`);
   });
 </script>
 
