439da752d16cbc9adba47e75c50d5d786fd151c2
VictorMRD <victormrd2002@hotmail.com>
2024-05-22 11:23:47 -0700

    Add delete action to dropdown

--- src/components/DataTableDropDown.vue
+++ src/components/DataTableDropDown.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-	import { MoreHorizontal } from 'lucide-vue-next';
-	import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-	import { Button } from '@/components/ui/button';
+  import { MoreHorizontal } from 'lucide-vue-next';
+  import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+  import { Button } from '@/components/ui/button';
+  import axios from "@/lib/axios";
+  import { useRouter, useRoute } from 'vue-router';
 
-	defineProps<{
-    resource: string
-    id: number
-	}>()
+  const props = defineProps<{
+    resource: string;
+    id: number;
+  }>();
+
+  const router = useRouter();
 
   function deleteElement(argument: any) {
-    console.log("Eliminar"+argument);
+    axios.delete(`/${props.resource}/${props.id}`)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch(error => {
+        console.error('Error deleting element:', error);
+      });
   }
 </script>
 
