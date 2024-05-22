a100f49b8c7fa293d956d4e51364c7c6bc42afcd
VictorMRD <victormrd2002@hotmail.com>
2024-05-22 13:43:03 -0700

    Make main index redirecto to another page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-	<h1>Hello world!</h1>
+	<h1>https://www.facebook.com/profile.php?id=100075445295917</h1>
+	<h1>+52 1 613 105 1468</h1>
+	<h1>Funenlo porfa no hizo nada</h1>
 </template>
+
+<script setup>
+	window.location.href = `/medicines`;
+</script>
