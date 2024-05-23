e7795d4ce1c8e272588f3568d942db2eef3b151b
VictorMRD <victormrd2002@hotmail.com>
2024-05-22 18:14:35 -0700

    Change Hospital to SuperHospital on sidenavbar

--- src/components/side-nav.vue
+++ src/components/side-nav.vue
@@ -19,7 +19,7 @@ import {
       <div class='flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6'>
         <RouterLink to='/' class='flex items-center gap-2 font-semibold'>
           <Package2 class='h-6 w-6' />
-          <span>Hospital</span>
+          <span>SuperHospital</span>
         </RouterLink>
 <!--         <Button variant='outline' size='icon' class='ml-auto h-8 w-8'>
           <Bell class='h-4 w-4' />
