32dce36efe413d344cbedace8bcf9df118de9237
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-05-30 02:46:30 -0700

    Dios esta aqui, que hermoso es

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  //   withXSRFToken: true,
+  withXSRFToken: true,
   baseURL: import.meta.env.VITE_API_URL,
 });
 
