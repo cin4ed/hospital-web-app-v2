9ed078cfce7eb9a9a739340f6a31a2e43fea05a6
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-05-30 00:33:52 -0700

    x

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,9 +4,6 @@ const instance = axios.create({
   withCredentials: true,
   //   withXSRFToken: true,
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Referer: "https://hospital.betweenframes.net",
-  },
 });
 
 export default instance;
